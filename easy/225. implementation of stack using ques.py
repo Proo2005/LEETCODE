c5cd805a3c6a41caa0f63475dccbb673a0fefc7b
Proo2005 <124602243+Proo2005@users.noreply.github.com>
2025-07-14 22:57:28 +0530

    easy

---
+++ easy/225. implementation of stack using ques.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+class MyStack:
+
+    def __init__(self):
+        self.q1 = deque()
+        self.q2 = deque()
+
+    def push(self, x: int) -> None:
+        self.q2.append(x)
+        # Transfer all elements from q1 to q2
+        while self.q1:
+            self.q2.append(self.q1.popleft())
+        # Swap the queues
+        self.q1, self.q2 = self.q2, self.q1
+
+    def pop(self) -> int:
+        return self.q1.popleft()
+
+    def top(self) -> int:
+        return self.q1[0]
+
+    def empty(self) -> bool:
+        return not self.q1

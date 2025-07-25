421550f89904a953e6df5856fa9da722ba230143
Proo2005 <124602243+Proo2005@users.noreply.github.com>
2025-07-25 17:52:59 +0530

    easy

--- easy/7609.to lower case.py
+++ easy/7609.to lower case.py
@@ -2,3 +2,22 @@
 class Solution:
     def toLowerCase(self, s: str) -> str:
         return s.lower()
+    
+
+
+
+
+##   or 
+
+
+
+
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        a=''
+        for i in s:
+            if ord(i)>=65 and ord(i)<=90:
+                a+=chr(ord(i)+32)
+            else:
+                a+=i
+        return a

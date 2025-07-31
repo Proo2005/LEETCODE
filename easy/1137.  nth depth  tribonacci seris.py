b0c3bd53566e064f5f9edbad2a3c4d28eca04a6f
Proo2005 <124602243+Proo2005@users.noreply.github.com>
2025-07-31 19:30:38 +0530

    easy

---
+++ easy/1137.  nth depth  tribonacci seris.py
@@ -0,0 +1,12 @@
+from functools import lru_cache
+
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        @lru_cache(None)
+        def tribo(k):
+            if k == 0:
+                return 0
+            if k == 1 or k == 2:
+                return 1
+            return tribo(k - 1) + tribo(k - 2) + tribo(k - 3)
+        return tribo(n)

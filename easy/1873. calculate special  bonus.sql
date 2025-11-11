1acf8b2c5d6267026212d5b60fe39cba66f01181
Proo2005 <124602243+Proo2005@users.noreply.github.com>
2025-11-11 20:31:26 +0530

    EASY

---
+++ easy/1873. calculate special  bonus.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT 
+    employee_id,
+    CASE 
+        WHEN name LIKE 'M%' OR employee_id % 2 = 0 THEN 0
+        ELSE salary
+    END AS bonus
+FROM Employees order by employee_id asc;

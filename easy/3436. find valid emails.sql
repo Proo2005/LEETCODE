d54b8ec6aebb55d273254548e21a2a5e86f258ae
Proo2005 <124602243+Proo2005@users.noreply.github.com>
2025-11-12 21:46:29 +0530

    easy

--- easy/3436. find valid emails.sql
+++ easy/3436. find valid emails.sql
@@ -1,1 +1,4 @@
- email REGEXP '^[A-Za-z0-9_]+@[A-Za-z]+\\.com$'
+# Write your MySQL query statement below
+select * from Users
+where  email REGEXP '^[A-Za-z0-9_]+@[A-Za-z]+\\.com$'
+order by  user_id

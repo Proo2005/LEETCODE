002ff0d9c999a90a3a0f6bb9deb74fb186dfd42f
Proo2005 <124602243+Proo2005@users.noreply.github.com>
2025-11-13 21:55:34 +0530

    easy

---
+++ easy/3570. find books with no avialable copies.sql
@@ -0,0 +1,16 @@
+SELECT 
+    lb.book_id,
+    lb.title,lb.author,lb.genre,lb.publication_year,
+    COUNT(br.record_id) AS current_borrowers
+FROM library_books lb
+JOIN borrowing_records br 
+    ON lb.book_id = br.book_id
+WHERE 
+    br.return_date IS NULL
+GROUP BY 
+    lb.book_id, lb.title, lb.total_copies
+HAVING 
+    lb.total_copies = COUNT(br.record_id)
+ORDER BY 
+    current_borrowers DESC,
+    lb.title ASC;

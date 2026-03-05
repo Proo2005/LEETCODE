e383a6010b8c3bf16b67903795a22172d986701f
Proo2005 <prochak1922@gmail.com>
2026-03-05 18:33:57 +0530

    hard  python

---
+++ python/hard/132.  palindrome string partioning II.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minCut(self, s: str) -> int:
+        n = len(s)
+        dp = [0]*n
+        pal = [[False]*n for _ in range(n)]
+
+        for i in range(n):
+            min_cut = i
+            for j in range(i+1):
+                if s[i] == s[j] and (i-j <= 2 or pal[j+1][i-1]):
+                    pal[j][i] = True
+                    if j == 0:
+                        min_cut = 0
+                    else:
+                        min_cut = min(min_cut, dp[j-1] + 1)
+
+            dp[i] = min_cut
+
+        return dp[-1]

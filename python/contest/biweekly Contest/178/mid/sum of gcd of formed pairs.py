b3dea0498c844936e30198a862081aae00671b77
Proo2005 <prochak1922@gmail.com>
2026-03-14 20:44:34 +0530

    python mid easy folder

---
+++ python/contest/biweekly Contest/178/mid/sum of gcd of formed pairs.py
@@ -0,0 +1,6 @@
+class Solution:
+    def firstUniqueEven(self, nums: list[int]) -> int:
+        for i in  nums:
+            if nums.count(i)==1 and i%2==0:
+                return i
+        return -1

8fb8e70d5a466e19641e58a3b2ea4ede8dd6dbc4
Proo2005 <prochak1922@gmail.com>
2026-03-15 15:59:44 +0530

    python easy

--- python/easy/836. rectangle overlap.py
+++ python/easy/836. rectangle overlap.py
@@ -1,4 +1,6 @@
-return (
+class Solution:
+    def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
+        return (
             rec1[0] < rec2[2] and
             rec1[2] > rec2[0] and
             rec1[1] < rec2[3] and

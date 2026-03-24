eded9ba4b1f1c525118106e07e65398897a8a796
Prodipta Chakraborty <prochak1922@example.com>
2026-03-24 10:30:59 +0530

    initAL

--- j.py
+++ j.py
@@ -1,20 +1,17 @@
 import os
 import shutil
 
-source = "hard"      # source folder
-destination = "sql/hard"    # destination folder
+source = "hard"     
+destination = "sql/hard"   
 
-# create destination folder if it does not exist
 os.makedirs(destination, exist_ok=True)
 
-# loop through all files in source
 for file in os.listdir(source):
     if file.endswith(".sql"):
         src_path = os.path.join(source, file)
         dst_path = os.path.join(destination, file)
-        
-        # move the file
+
         shutil.move(src_path, dst_path)
         print(f"Moved: {file}")
 
-print("Transfer complete!")
+

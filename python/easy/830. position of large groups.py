edecb35d7ee4b5ff0cabbd45f03b6752055b0a5e
Proo2005 <prochak1922@gmail.com>
2026-04-17 21:17:28 +0530

    python easy

---
+++ python/easy/830. position of large groups.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def largeGroupPositions(self, s: str) -> List[List[int]]:
+        res = []
+        i = 0
+
+        while i < len(s):
+            j = i
+
+    
+            while j < len(s) and s[j] == s[i]:
+                j += 1
+
+
+            if j - i >= 3:
+                res.append([i, j - 1])
+
+            i = j  
+
+        return res

d49552f95fa63337391beddd0d2c19e6b7dda7e5
Proo2005 <prochak1922@gmail.com>
2026-05-02 23:13:36 +0530

    python mid

--- python/mid/788. rotated digits.py
+++ python/mid/788. rotated digits.py
@@ -1,10 +1,20 @@
-
 class Solution:
     def rotatedDigits(self, n: int) -> int:
-        a=[2,5,6,9]
-        c=0
-        for i in range(1,n+1):
-            for j in str(i):
-                if int(j) in a:
-                    c+=1
-        return c 
+        good = 0
+
+        for i in range(1, n + 1):
+            s = str(i)
+            valid = True
+            changed = False
+
+            for ch in s:
+                if ch in '347':
+                    valid = False
+                    break
+                if ch in '2569':
+                    changed = True
+
+            if valid and changed:
+                good += 1
+
+        return good

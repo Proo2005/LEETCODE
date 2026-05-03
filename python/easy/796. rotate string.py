27b4b39b14493bba6ebae60b03fcca178e782002
Proo2005 <prochak1922@gmail.com>
2026-05-03 12:04:14 +0530

    python easy

--- python/easy/796. rotate string.py
+++ python/easy/796. rotate string.py
@@ -7,3 +7,7 @@ class Solution:
             if goal==a:
                 return True
         return False
+
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        return len(s) == len(goal) and goal in (s + s)

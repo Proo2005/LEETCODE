cbe597bea639dc02c8c5ae1959ccf5956d794581
Proo2005 <prochak1922@gmail.com>
2026-06-02 23:48:25 +0530

    python easy

---
+++ python/easy/2144.minimum cost of buying candies with discount.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minimumCost(self, cost: List[int]) -> int:
+        cost.sort(reverse=True)
+
+        free = 0
+        for i in range(2, len(cost), 3):
+            free += cost[i]
+
+        return sum(cost) - free
